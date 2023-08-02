458b9967f635bd9416c8b81b53dbb86cc5f1d70c
IlyuAfanasev <gecktor.86@mail.ru>
2023-08-02 22:06:25 +0400

    intermediate commit

---
+++ src/main/java/ru/hogwarts/school/model/Faculty.java
@@ -0,0 +1,31 @@
+package ru.hogwarts.school.model;
+
+public class Faculty {
+
+    private Long id;
+    private final String name;
+
+    private final String color;
+
+    public Faculty(Long id, String name, String color) {
+        this.id = id;
+        this.name = name;
+        this.color = color;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}
---
+++ src/main/java/ru/hogwarts/school/model/Student.java
@@ -0,0 +1,49 @@
+package ru.hogwarts.school.model;
+
+public class Student {
+    private Long id;
+    private final String name;
+
+    private final int age;
+
+    public Student(Long id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
