c731110046089dc5c7cba2d0b09bd647612560d8
IlyuAfanasev <gecktor.86@mail.ru>
2023-08-04 21:25:36 +0400

    Add interface FacultyService, StudentService, method clear

--- src/main/java/ru/hogwarts/school/service/FacultyService.java
+++ src/main/java/ru/hogwarts/school/service/FacultyService.java
@@ -1,66 +1,23 @@
 package ru.hogwarts.school.service;
 
-import org.springframework.stereotype.Service;
 import ru.hogwarts.school.model.Faculty;
-import ru.hogwarts.school.model.Student;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.Collection;
 
-@Service
-public class FacultyService {
+public interface FacultyService {
 
-    private final Map<Long, Faculty> faculties;
+    Faculty createFaculty(Faculty faculty);
 
-    private Long facultyId = 1L;
+    Faculty getFaculty(Long id);
 
-    public FacultyService() {
-        this.faculties = new HashMap<>();
-    }
+    Faculty editFaculty(Faculty faculty);
 
-    public Faculty createFaculty(Faculty faculty) {
-        faculty.setId(facultyId);
-        faculties.put(facultyId, faculty);
-        facultyId++;
-        return faculty;
+    Faculty deleteFaculty(Long id);
 
-    }
+    Collection<Faculty> getAll();
 
-    public Faculty getFaculty(Long id) {
+    Collection<Faculty> getFacultiesByColor(String color);
 
-        return faculties.get(id);
-
-    }
-
-    public Faculty editFaculty(Faculty faculty) {
-        if (faculties.containsKey(faculty.getId())) {
-
-             faculties.put(faculty.getId(), faculty);
-
-            return faculties.get(faculty.getId());
-        }
-        return null;
-    }
-
-    public Faculty deleteFaculty(Long id) {
-
-
-            return faculties.remove(id);
-
-
-    }
-
-    public Collection<Faculty> getAll() {
-
-        return Collections.unmodifiableList(new ArrayList<>(faculties.values()));
-    }
-
-    public Collection<Faculty> getFacultiesByColor(String color) {
-
-        return faculties.values().stream()
-                .filter(f -> f.getColor().equals(color))
-                .collect(Collectors.toList());
-    }
-
+    void clear();
 
 }
