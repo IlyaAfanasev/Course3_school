e5d8e1c6b6df2f629f9db0f20e137a3951291767
IlyuAfanasev <gecktor.86@mail.ru>
2023-08-13 22:24:22 +0400

    add Avatar's Model/Service/Repository/Controller; add method uploadAvatar

---
+++ src/main/java/ru/hogwarts/school/model/Avatar.java
@@ -0,0 +1,102 @@
+package ru.hogwarts.school.model;
+
+import org.springframework.beans.factory.annotation.Value;
+
+import javax.persistence.*;
+import java.util.Arrays;
+import java.util.Objects;
+
+@Entity
+public class Avatar {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String filePath;
+    private long fileSize;
+    private String mediaType;
+    private byte[] data;
+
+    @Value("${path.to.avatars.folder}")
+    private String avatarsDir;
+    @OneToOne
+    @JoinColumn(name = "student_id")
+    private Student student;
+
+    public Avatar() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public long getFileSize() {
+        return fileSize;
+    }
+
+    public void setFileSize(long fileSize) {
+        this.fileSize = fileSize;
+    }
+
+    public String getMediaType() {
+        return mediaType;
+    }
+
+    public void setMediaType(String mediaType) {
+        this.mediaType = mediaType;
+    }
+
+    public byte[] getData() {
+        return data;
+    }
+
+    public void setData(byte[] data) {
+        this.data = data;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    @Override
+    public String toString() {
+        return "Avatar{" +
+                "id=" + id +
+                ", filePath='" + filePath + '\'' +
+                ", fileSize=" + fileSize +
+                ", mediaType='" + mediaType + '\'' +
+                ", data=" + Arrays.toString(data) +
+                ", student=" + student +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Avatar avatar = (Avatar) o;
+        return Objects.equals(filePath, avatar.filePath);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(filePath);
+    }
+}
+
+
